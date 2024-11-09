f60bb1dd092996a986c30895761dff542233fb68
olehmarushchak <westernpc22@gmail.com>
2024-11-09 21:42:40 +0100

    add gps

--- web-ar/src/components/AR/Hiro.js
+++ web-ar/src/components/AR/Hiro.js
@@ -3,29 +3,20 @@ import React from "react";
 export const Hiro = () => {
   return (
     <>
-      {/* A-Frame сцена з AR.js */}
-      <a-scene embedded arjs="sourceType: webcam; debugUIEnabled: false;">
-        {/* Маркер, на якому відображатиметься 3D об'єкт */}
-        <a-marker
-          preset="pattern"
-          url="https://raw.githubusercontent.com/lbelfield/augmented-reality/master/src/components/trainTicket/train-ticket.patt"
-        >
-          {/* 3D об'єкт, який з'являється на маркері */}
-          
-          <a-entity
-            geometry="primitive: box; height: 1; depth: 1; rotation: 90 0 0;"
-            material="color: blue"
-          />
+      <a-scene
+        vr-mode-ui="enabled: false"
+        arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false"
+        renderer="antialias: true; alpha: true"
+      >
+        <a-camera gps-new-camera="gpsMinDistance: 5"></a-camera>
 
-          {/* <a-entity
-            position="0 0 0"
-            scale="1 1 1"
-            gltf-model="backpack/backpack.glb"
-          ></a-entity> */}
-        </a-marker>
-
-        {/* Камера для AR сцени */}
-        <a-camera />
+        <a-entity
+          material="color: red"
+          geometry="primitive: box"
+          gps-new-entity-place="latitude: 48.1299; longitude: 14.2331"
+          scale="10 10 10"
+        ></a-entity>
+        
       </a-scene>
     </>
   );
